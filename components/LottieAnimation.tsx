349aa7bf62f441ecfae0e39a77cd62f434bcb834
Tomiwa Adelae <thetommedia@gmail.com>
2025-04-18 09:49:51 +0100

    Updated the Lottie Animation

---
+++ components/LottieAnimation.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import Lottie from "lottie-react";
+import animationData from "@/public/assets/animations/house-animation.json";
+
+const LottieAnimation = () => {
+	return <Lottie animationData={animationData} />;
+};
+
+export default LottieAnimation;
--- components/Showcase.tsx
+++ components/Showcase.tsx
@@ -1,6 +1,4 @@
-"use client";
-import Lottie from "lottie-react";
-import animationData from "@/public/assets/animations/house-animation.json";
+import LottieAnimation from "./LottieAnimation";
 
 const Showcase = ({
 	headline,
@@ -15,9 +13,7 @@ const Showcase = ({
 		<div className="min-h-[70vh] bg-white pt-16 flex items-center justify-center">
 			<div className="container grid grid-cols-1 gap-8 md:grid-cols-2">
 				<div className="flex flex-col items-start w-full justify-center">
-					<h1
-						className={`font-bold uppercase text-4xl md:text-5xl lg:text-6xl`}
-					>
+					<h1 className="font-bold uppercase text-4xl md:text-5xl lg:text-6xl">
 						{headline}
 					</h1>
 					<p className="text-base text-muted-foreground mt-4 mb-6">
@@ -26,7 +22,7 @@ const Showcase = ({
 					{details}
 				</div>
 				<div className="flex items-center justify-center">
-					<Lottie animationData={animationData} />
+					<LottieAnimation />
 				</div>
 			</div>
 		</div>
