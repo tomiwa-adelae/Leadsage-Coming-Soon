ba039661d7f1867a854e02e2079608db8d747903
Tomiwa Adelae <thetommedia@gmail.com>
2025-05-11 16:14:27 +0100

    Added the navlink to the header for mobile

--- components/MobileNavbar.tsx
+++ components/MobileNavbar.tsx
@@ -1,20 +1,88 @@
+// "use client";
+
+// import { Button } from "@/components/ui/button";
+// import {
+// 	Sheet,
+// 	SheetClose,
+// 	SheetContent,
+// 	SheetTrigger,
+// } from "@/components/ui/sheet";
+// import { navLinks } from "@/constants";
+// import Image from "next/image";
+// import Link from "next/link";
+
+// export function MobileNavbar({ links }: { links: boolean }) {
+// 	return (
+// 		<Sheet>
+// 			<SheetTrigger asChild>
+// 				<Button
+// 					variant="ghost"
+// 					className="hover:bg-transparent"
+// 					size={"icon"}
+// 				>
+// 					<Image
+// 						src={"/assets/icons/menu.svg"}
+// 						alt={"Menu Icon"}
+// 						width={1000}
+// 						height={1000}
+// 						className="w-[40px] invert h-[40px] md:w-[40px] md:h-[40px] "
+// 					/>
+// 				</Button>
+// 			</SheetTrigger>
+// 			<SheetContent
+// 				className="bg-[#204B41] text-white  h-screen"
+// 				side={"left"}
+// 			>
+// 				<Link href={"/"}>
+// 					<Image
+// 						src={"/assets/images/logo.png"}
+// 						alt={"LeadSage Logo"}
+// 						width={1000}
+// 						height={1000}
+// 						className="w-40 lg:w-52 object-cover"
+// 					/>
+// 				</Link>
+// 				{links && (
+// 					<nav className="flex flex-col gap-4 mt-4 container">
+// 						{navLinks.map(({ label, slug }, index) => {
+// 							return (
+// 								<SheetClose asChild key={index}>
+// 									<Link
+// 										href={slug}
+// 										className={`group flex items-center justify-start gap-2 group/sidebar py-2 text-white hover:text-gray-200`}
+// 										onClick={() => {}}
+// 									>
+// 										<span className="text-sm group-hover/sidebar:translate-x-1 transition duration-150 whitespace-pre inline-block !p-0 !m-0 uppercase font-medium">
+// 											{label}
+// 										</span>
+// 									</Link>
+// 								</SheetClose>
+// 							);
+// 						})}
+// 					</nav>
+// 				)}
+// 			</SheetContent>
+// 		</Sheet>
+// 	);
+// }
+
 "use client";
 
 import { Button } from "@/components/ui/button";
 import {
-	Sheet,
-	SheetClose,
-	SheetContent,
-	SheetTrigger,
-} from "@/components/ui/sheet";
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerTrigger,
+} from "@/components/ui/drawer";
 import { navLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 
 export function MobileNavbar({ links }: { links: boolean }) {
 	return (
-		<Sheet>
-			<SheetTrigger asChild>
+		<Drawer>
+			<DrawerTrigger asChild>
 				<Button
 					variant="ghost"
 					className="hover:bg-transparent"
@@ -28,11 +96,8 @@ export function MobileNavbar({ links }: { links: boolean }) {
 						className="w-[40px] invert h-[40px] md:w-[40px] md:h-[40px] "
 					/>
 				</Button>
-			</SheetTrigger>
-			<SheetContent
-				className="bg-[#204B41] text-white  h-screen"
-				side={"left"}
-			>
+			</DrawerTrigger>
+			<DrawerContent>
 				<Link href={"/"}>
 					<Image
 						src={"/assets/images/logo.png"}
@@ -46,7 +111,7 @@ export function MobileNavbar({ links }: { links: boolean }) {
 					<nav className="flex flex-col gap-4 mt-4 container">
 						{navLinks.map(({ label, slug }, index) => {
 							return (
-								<SheetClose asChild key={index}>
+								<DrawerClose asChild key={index}>
 									<Link
 										href={slug}
 										className={`group flex items-center justify-start gap-2 group/sidebar py-2 text-white hover:text-gray-200`}
@@ -56,12 +121,12 @@ export function MobileNavbar({ links }: { links: boolean }) {
 											{label}
 										</span>
 									</Link>
-								</SheetClose>
+								</DrawerClose>
 							);
 						})}
 					</nav>
 				)}
-			</SheetContent>
-		</Sheet>
+			</DrawerContent>
+		</Drawer>
 	);
 }
--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -7,44 +7,63 @@ import { MobileNavbar } from "../MobileNavbar";
 
 const Header = ({ links }: { links: boolean }) => {
 	return (
-		<header className="container py-4 flex items-center justify-between gap-4">
-			<Link href={"/"}>
-				<Image
-					src={"/assets/images/logo.png"}
-					alt={"LeadSage Logo"}
-					width={1000}
-					height={1000}
-					className="w-40 lg:w-52 object-cover"
-				/>
-			</Link>
-			<div className="flex-1 md:flex items-center justify-end gap-6">
-				{links && (
-					<nav className="hidden md:flex items-center justify-end gap-6">
-						{navLinks.map((link, index) => {
-							return (
-								<Link
-									key={index}
-									className={cn(
-										"uppercase text-xs lg:text-sm font-medium text-white"
-									)}
-									href={link.slug}
-								>
-									{link.label}
-								</Link>
-							);
-						})}
-					</nav>
-				)}
-				<Button className="hidden md:flex" asChild size={"md"}>
-					<Link href="/#waitlist">Join</Link>
-				</Button>
-				<div className="md:hidden flex items-center justify-end gap-2 md:gap-4">
-					<Button asChild size={"md"}>
+		<header className="container py-4">
+			<div className="flex items-center justify-between gap-4">
+				<Link href={"/"}>
+					<Image
+						src={"/assets/images/logo.png"}
+						alt={"LeadSage Logo"}
+						width={1000}
+						height={1000}
+						className="w-40 lg:w-52 object-cover"
+					/>
+				</Link>
+				<div className="flex-1 md:flex items-center justify-end gap-6">
+					{links && (
+						<nav className="hidden md:flex items-center justify-end gap-6">
+							{navLinks.map((link, index) => {
+								return (
+									<Link
+										key={index}
+										className={cn(
+											"uppercase text-xs lg:text-sm font-medium text-white"
+										)}
+										href={link.slug}
+									>
+										{link.label}
+									</Link>
+								);
+							})}
+						</nav>
+					)}
+					<Button className="hidden md:flex" asChild size={"md"}>
 						<Link href="/#waitlist">Join</Link>
 					</Button>
-					{/* {links && <MobileNavbar links={links} />} */}
+					<div className="md:hidden flex items-center justify-end gap-2 md:gap-4">
+						<Button asChild size={"md"}>
+							<Link href="/#waitlist">Join</Link>
+						</Button>
+						{/* {links && <MobileNavbar links={links} />} */}
+					</div>
 				</div>
 			</div>
+			{links && (
+				<nav className="mt-6 flex items-center justify-center gap-4 md:hidden">
+					{navLinks.map((link, index) => {
+						return (
+							<Link
+								key={index}
+								className={cn(
+									"uppercase text-xs lg:text-sm font-medium text-white"
+								)}
+								href={link.slug}
+							>
+								{link.label}
+							</Link>
+						);
+					})}
+				</nav>
+			)}
 		</header>
 	);
 };
